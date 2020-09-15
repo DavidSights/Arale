eae95ba3dee8c6d382587211793556abb05b416c
David Seitz Jr <davidsights@me.com>
2020-09-14 22:23:32 -0500

    Added Arale files

---
+++ Sources/FlexibleHeader/AraleHeaderViewDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AraleHeaderViewDelegate.swift
+//  Arale
+//
+//  Created by Zulwiyoza Putra on 13/11/18.
+//  Copyright © 2018 Zulwiyoza Putra. All rights reserved.
+//
+
+#if !os(macOS)
+
+import Foundation
+
+public protocol AraleHeaderViewDelegate: NSObjectProtocol {
+    func headerViewWillResizeFrame(_ headerView: AraleHeaderView)
+    func headerViewDidResizeFrame(_ headerView: AraleHeaderView)
+    func headerViewDidReachMaxHeight(_ headerView: AraleHeaderView)
+}
+
+public extension AraleHeaderView {
+    func headerViewWillResizeFrame(_ headerView: AraleHeaderView) { return }
+    func headerViewDidResizeFrame(_ headerView: AraleHeaderView) { return }
+    func headerViewDidReachMaxHeight(_headerView: AraleHeaderView) { return }
+}
+
+#endif
